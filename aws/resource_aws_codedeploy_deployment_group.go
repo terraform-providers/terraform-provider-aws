17d4843dd7a2269e4948c26120956d51a98d690e
philipdickinson <philip.jerome.dickinson@gmail.com>
2019-11-05 08:09:51 -0800

    Correct docstrings on TagSetToMap functions.

--- aws/resource_aws_codedeploy_deployment_group.go
+++ aws/resource_aws_codedeploy_deployment_group.go
@@ -1213,7 +1213,7 @@ func onPremisesTagFiltersToMap(list []*codedeploy.TagFilter) []map[string]interf
 	return result
 }
 
-// ec2TagSetToMap converts lists of tag filters into a [][]map[string]string.
+// ec2TagSetToMap converts lists of tag filters into a [][]map[string]interface.
 func ec2TagSetToMap(tagSet *codedeploy.EC2TagSet) []map[string]interface{} {
 	var result []map[string]interface{}
 	if tagSet == nil {
@@ -1235,7 +1235,7 @@ func ec2TagSetToMap(tagSet *codedeploy.EC2TagSet) []map[string]interface{} {
 	return result
 }
 
-// onPremisesTagSetToMap converts lists of tag filters into a [][]map[string]string.
+// onPremisesTagSetToMap converts lists of tag filters into a [][]map[string]interface.
 func onPremisesTagSetToMap(tagSet *codedeploy.OnPremisesTagSet) []map[string]interface{} {
 	var result []map[string]interface{}
 	if tagSet == nil {
