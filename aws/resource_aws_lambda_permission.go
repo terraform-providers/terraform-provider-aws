5ad5748edc0b435fc22f70fcf9dbad7c65442b17
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-11-26 00:15:45 +0900

    fix: correct imported function_name of lambda_permission

--- aws/resource_aws_lambda_permission.go
+++ aws/resource_aws_lambda_permission.go
@@ -446,7 +446,7 @@ func resourceAwsLambdaPermissionImport(d *schema.ResourceData, meta interface{})
 		return nil, err
 	}
 
-	d.Set("function_name", getFunctionOutput.Configuration.FunctionArn)
+	d.Set("function_name", getFunctionOutput.Configuration.FunctionName)
 	d.Set("statement_id", statementId)
 	if qualifier != "" {
 		d.Set("qualifier", qualifier)
